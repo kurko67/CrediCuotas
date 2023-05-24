23d8aed4b6d73ce18da6c317eb54a8735af820b2
kurko67 <maxi.alaniz@gmail.com>
2023-05-24 09:44:45 -0300

    agregando login page and admin

--- src/main/java/com/bolsadeideas/springboot/app/controllers/AdminController.java
+++ src/main/java/com/bolsadeideas/springboot/app/controllers/AdminController.java
@@ -2,16 +2,17 @@ package com.bolsadeideas.springboot.app.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class AdminController {
 
 
-    @GetMapping("/administrador")
+    @RequestMapping("/administrador")
     public String index_admin() {
 
-        return "/administrator/administrator";
+        return "administrator/admin";
     }
 
 
